2ec1fafa1b245b64775977de05b82b9e3425c100
Prabh Kahlon <prabhk@sfu.ca>
2021-01-10 13:06:57 -0800

    centered image

--- client/src/components/GraphTile.js
+++ client/src/components/GraphTile.js
@@ -3,7 +3,7 @@ import someGraph from '../Canada-Confirmed.png'
 
 export default function GraphTile() {
     return (
-        <div className="mx-auto p-2" style={{width: "90vw", height: "400px"}}>
+        <div className="mx-auto p-2 text-center" style={{width: "90vw", height: "400px"}}>
             <img className="img-fluid img-thumbnail" src={someGraph} alt="graph" style={{background: "none"}}></img>
         </div>
     )
