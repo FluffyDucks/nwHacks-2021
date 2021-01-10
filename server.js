f3e8e23d494ac4196c086eb19203c253fe22487b
David Yeung <dya40@sfu.ca>
2021-01-10 12:51:17 -0800

    api endpoint mongo model created

--- server.js
+++ server.js
@@ -18,15 +18,28 @@ app.get('/api/cases/:country', async (req,res) => {
     }
 })
 
-app.get('/api/MLmodel/:countryName', async (req,res) => {
+app.get('/api/MLmodel/insert/:country', async (req,res) => {
     console.log("model before");
-    let data = await modelPredictions(req.params.countryName)
+    countryName = req.params.country;
+    let data = await modelPredictions(countryName)
     console.log("model after")
     
     res.json(JSON.parse(data));
-    insertModelData(data);
+    insertModelData(data, countryName);
     console.log("data inserted");
 });
+
+app.get('/api/MLmodel/:country', async (req,res) => {
+    console.log("model before");
+    country = req.params.countryName;
+    if(req.params.country) {
+        res.json(await getModelPredictions(req.params.country));
+    } else {
+        res.json({Message: "Country Not Found"});
+    }
+});
+
+
 
 async function modelPredictions(country){
     return new Promise((resolve, reject) => {
@@ -140,11 +153,11 @@ async function insertDB() {
 
 insertDB();
 
-async function insertModelData(data) {
+async function insertModelData(data, country) {
     console.log(data);
     let today = new Date();
 
-    let name = `model-${today.getFullYear()}-${today.getMonth() + 1}-${today.getDate()}`;
+    let name = `${country}-${today.getFullYear()}-${today.getMonth() + 1}-${today.getDate()}`;
 
     //if the table exists, insert covid data into said table
     if (await createTable(name) == 0) {
@@ -161,11 +174,8 @@ async function insertModelData(data) {
     }
 }
 
-
 async function getCountriesFromDB() {
     return new Promise(async (resolve, reject) => {
-
-
         let today = new Date();
         let name = `${today.getFullYear()}-${today.getMonth() + 1}-${today.getDate()}`;
 
@@ -193,6 +203,25 @@ async function getCasesFromDB(country) {
                 if (err) throw err;
                 let dbo = db.db("mydb");
                 dbo.collection(`${name}`).findOne({Country: country}, { projection: {_id: 0, CountryCode: 0, Slug: 0, Premium: 0, Date: 0} }, function (err, res) {
+                    if (err) throw err;
+                    db.close();
+                    resolve(res);
+                });
+            });
+        }
+    });
+}
+
+async function getModelPredictions(country){
+    return new Promise(async (resolve, reject) => {
+        let today = new Date();
+        let name = `${country}-${today.getFullYear()}-${today.getMonth() + 1}-${today.getDate()}`;
+
+        if (await createTable(name)) {
+            MongoClient.connect(url, function (err, db) {
+                if (err) throw err;
+                let dbo = db.db("mydb");
+                dbo.collection(`${name}`).find(function (err, res) {
                     if (err) throw err;
                     db.close();
                     resolve(res);
